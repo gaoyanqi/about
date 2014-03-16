78e456eaa9fc580f0b28406b0c36307378b80dc2
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-03-16 13:13:42 +0800

    1.重构：重构项目代码

--- js/time-calculator.js
+++ js/time-calculator.js
@@ -1,3 +1,14 @@
+/**
+ * Copyright(c)2013,zhangchunsheng,www.zhangchunsheng.me
+ * Version: 1.0
+ * Author: zhangchunsheng
+ * Date: 2014-03-16
+ * Description: time-calculator
+ * Modification:
+ *      甲、乙、丙、丁、戊、己、庚、辛、壬、癸 甲（jiǎ）、乙（yǐ）、丙（bǐng）、丁（dīng）、戊（wù）、己（jǐ）、庚（gēng）、辛（xīn）、壬（rén）、癸（guǐ）
+ *      子、丑、寅、卯、辰、巳、午、未、申、酉、戌、亥 子（zǐ）、丑（chǒu）、寅（yín）、卯（mǎo）、辰（chén）、巳（sì）、午（wǔ）、未（wèi）、申（shēn）、酉（yǒu）、戌（xū）、亥（hài）
+ * 		甲午年（马年）丁卯月丙戌日 农历二月十六
+ */
 var birth = new Date('1990/05/13 01:00:00');
 var present = new Date();
 var death = new Date('2067/08/09 01:00:00');
